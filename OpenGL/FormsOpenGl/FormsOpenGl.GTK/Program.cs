6b9d34f2fe69fe88f244072d15598e5444581db6
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2017-08-16 13:18:20 +0200

    Updated NuGet and added new OpenGL Sample

---
+++ OpenGL/FormsOpenGl/FormsOpenGl.GTK/Program.cs
@@ -0,0 +1,22 @@
+﻿using FormsOpenGl.Shared;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
+
+namespace FormsOpenGl.GTK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gtk.Application.Init();
+            Forms.Init();
+
+            var app = new App();
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("FormsOpenGl");
+            window.Show();
+            Gtk.Application.Run();
+        }
+    }
+}
---
+++ OpenGL/FormsOpenGl/FormsOpenGl.Shared/Application.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace FormsOpenGl.Shared
+{
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage = new OpenGlView(); 
+        }
+    }
+}
